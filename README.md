ae0d6ba11e04beb8bf7ad8bfc64ef57c0911b56d
Dmitriusan <dmitriusan@gmail.com>
2015-09-01 15:37:00 +0300

    made script parameterizable

--- README.md
+++ README.md
@@ -1,16 +1,52 @@
 # backup-lvm-fsa-snapshot
-create snapshot of LVM volume and back it up using filesystem archiver. 
+Create snapshot of LVM volume and back it up using filesystem archiver. 
 Snapshot volume is temporarily allocated on partition with backups.
+Old snapshots are removed (max counts of daily and weekly dumps are 
+configurable). Script is intended to be executed once every day.  
 
 
 ## Beware!
-1. Never point backup file destination to the same partition that is being 
-backed up. Snapshot space will be overfilled.
-2. Never reboot computer without removing snapshot volume. On next boot,
+1. **Never** point backup file destination (--backup-dest-dir) to the same partition that is being 
+backed up. **Snapshot space will be overfilled.**
+1. **Never** point tmp snaphot file destination (--lvm-snapshot-tmp-file-dir) to the same partition that is being 
+backed up. **Entire filesystem will hang!!!**
+1. Never reboot computer without removing snapshot volume. On next boot,
 temporary loop device will not be available, so operating system will fail
 to boot.
-3. If you did not follow advice 2. and rebooted while performing backup, 
-you can fix everything by booting from live cd and running 
-```
+1. If you did not follow advice 2. and rebooted while performing backup, 
+you can fix everything by booting from live cd and running ```
 vgreduce --removemissing --force <your vg name>
+```
+
+## What script does
+- Script installs fsarchiver package if it is not installed
+- Script creates destination directory at --backup-dest-dir for storing backups 
+if it does not exist 
+- Script creates 15 GB tmp file in directory, specified via --lvm-snapshot-tmp-file-dir . 
+If arg --tmp-dir-is-local is specified,
+ file is created using fallocate call (fast, but applicable only to local filesystem). 
+ Otherwise, if --tmp-dir-is-remote parameter is passed, temporary file is created 
+ using dd.
+- Then script creates a loop device on tmp file allocated earlier
+- Then script creates an LVM physical volume for volume group specified via --source-lvm-volume-group.
+ Physical volume is created on the loop device created beforehand.
+- Then script creates an LVM snapshot for LVM logical volume specified via 
+ --source-lvm-volume-group and --source-lvm-logical-volume options
+- Then script creates a dump of snapshot volume. Backup is created via fsarchiver. 
+Backup is compressed, compression uses all available CPUs, compression levels 
+(see http://www.fsarchiver.org/Compression) are defined using --compression-level
+ parameter.
+- every Sunday, a weekly dump is created. On other days a daily dump is created.
+ Limits of weekly and daily dump files are managed separately and 
+ are defined using --daily-backup-max-count and
+ --weekly-backup-max-count properties. Oldest dump files that exceed this limit are deleted.
+ E.g. 7 dayly backups and 4 weekly backups mean that one has daily backups for the last week
+ and weekly backups for the last month. 
+- After backup is finished, script removes physical volume, loop device and tmp file created earlier.
+- Then script removes old dumps (exceeding limits specified via --daily-backup-max-count 
+ and --weekly-backup-max-count
+
+## Example of command line to run backup
+```
+--compression-level 5
 ```
---
+++ backup-lvm-fsa-snapshot.sh
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+
+export SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Parse script args ( thanks http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash )
+while [[ $# -ge 1 ]]; do
+  key="$1"
+
+  case ${key} in
+    -v|--verbose)
+      set -x
+    ;;
+
+    --backup-dest-dir)
+      BACKUPS_AUTO_CREATED_LOCATION="$2"
+      shift
+    ;;
+
+    --source-lvm-volume-group)
+      LVM_ROOT_FS_VG="$2"
+      shift
+    ;;
+
+    --source-lvm-logical-volume)
+      LVM_ROOT_FS_LV="$2"
+      shift
+    ;;
+
+    --lvm-snapshot-tmp-file-dir)
+      LVM_SNAP_TMP_FILE_DIR="$2"
+      shift
+    ;;
+
+    --tmp-dir-is-local)     # Use fallocate to create tmp file
+      TMP_DIR_TYPE="local"
+    ;;
+
+    --tmp-dir-is-remote)    # On remote dir, fallocate is not available, use dd to create tmp file
+      TMP_DIR_TYPE="remote"
+    ;;
+
+    --daily-backup-max-count)
+      DAILY_BACKUP_MAX_COUNT="$2"
+      shift
+    ;;
+
+    --weekly-backup-max-count)
+      WEEKLY_BACKUP_MAX_COUNT="$2"
+      shift
+    ;;
+
+    --compression-level)
+      COMPRESSION_LEVEL="$2"
+      shift
+    ;;
+
+    -h|--help)
+      echo "Supported options:"
+      echo "-v|--verbose"
+      echo "-h|--help"
+      echo "--backup-dest-dir <place to store backups>"
+      echo "--source-lvm-volume-group <volume group name>"
+      echo "--source-lvm-logical-volume <logical volume name>"
+      echo "--lvm-snapshot-tmp-file-dir <place to store tmp file for snapshot writes>"
+      echo "--tmp-dir-is-local | --tmp-dir-is-remote"
+      echo "--daily-backup-max-count <number>"
+      echo "--weekly-backup-max-count <number>"
+      echo "--compression-level <number>"
+      exit 1
+    ;;
+
+    *)
+      # unknown option
+      echo "Unknown option $key"
+      exit 1
+    ;;
+  esac
+  shift
+done
+
+CPU_COUNT=`grep -c ^bogomips /proc/cpuinfo`
+
+# Check arguments
+[ -z "$BACKUPS_AUTO_CREATED_LOCATION" ] && echo "--backup-dest-dir not specified" && exit 1
+[ -z "$LVM_ROOT_FS_VG" ] && echo "--source-lvm-volume-group not specified" && exit 1
+[ -z "$LVM_ROOT_FS_LV" ] && echo "--source-lvm-logical-volume not specified" && exit 1
+[ -z "$LVM_SNAP_TMP_FILE_DIR" ] && echo "--lvm-snapshot-tmp-file-dir not specified" && exit 1
+[ -z "$TMP_DIR_TYPE" ] && echo "Either --tmp-dir-is-local or --tmp-dir-is-remote should be specified" && exit 1
+[ -z "$DAILY_BACKUP_MAX_COUNT" ] && echo "--daily-backup-max-count not specified" && exit 1
+[ -z "$WEEKLY_BACKUP_MAX_COUNT" ] && echo "--weekly-backup-max-count not specified" && exit 1
+[ -z "$COMPRESSION_LEVEL" ] && echo "--compression-level not specified" && exit 1
+
+# install fsarchiver
+apt-get -y update
+apt-get -y install fsarchiver
+
+
+# Detect day of week (1..7); 1 is Monday
+DOW=$(date +%u)
+
+# Generate filename
+TIMESTAMP=`date +%F_%H-%M-%S`
+DUMP_PREFIX=${LVM_ROOT_FS_VG}-${LVM_ROOT_FS_LV}
+WEEKLY_PREFIX=${DUMP_PREFIX}-dump-weekly
+DAILY_PREFIX=${DUMP_PREFIX}-dump-daily
+if [ "$DOW" -eq "7" ]; then
+  BACKUP_NAME=${WEEKLY_PREFIX}-${TIMESTAMP}.fsa
+else
+  BACKUP_NAME=${DAILY_PREFIX}-${TIMESTAMP}.fsa
+fi
+
+SNAP_NAME=${LVM_ROOT_FS_LV}_snapshot
+BACKUP_DIR=${BACKUPS_AUTO_CREATED_LOCATION}/${DUMP_PREFIX}-dumps
+if [ ! -d ${BACKUP_DIR} ]; then
+  mkdir -p ${BACKUP_DIR}
+fi
+
+# Check for previous unfinished backup
+OLD_LOOP_DEV=$(losetup -a | grep ${LVM_SNAP_TMP_FILE_DIR} | cut -d : -f 1)
+
+if [[ ! -z ${OLD_LOOP_DEV} ]]; then
+  echo "Looks like previous backup was not properly finished, exiting. Need manual intervention"
+  exit 1
+fi
+
+# Create tmp file for snapshot
+TMP_LVM_SNAPSHOT_FILE=${LVM_SNAP_TMP_FILE_DIR}/tmp_lvm_snapshot.img
+
+if [ ${TMP_DIR_TYPE} -eq "local" ]; then # Use fallocate - only local filesystem
+  fallocate -l 16G ${TMP_LVM_SNAPSHOT_FILE}
+  RC=$?;
+else: # Use dd - universal method
+  dd if=/dev/zero of=${TMP_LVM_SNAPSHOT_FILE} bs=16M count=1024
+  RC=$?;
+fi
+
+if [[ ${RC} != 0 ]]; then
+  echo "Can not create tmp file for LVM snapshot"
+  exit ${RC};
+fi
+
+# Create loopback device first
+LOOPBACK_DEV=`losetup -f --show ${TMP_LVM_SNAPSHOT_FILE}`
+
+pvcreate ${LOOPBACK_DEV}
+vgextend ${LVM_ROOT_FS_VG} ${LOOPBACK_DEV}
+lvcreate -s -n ${SNAP_NAME} -L 15g ${LVM_ROOT_FS_VG}/${LVM_ROOT_FS_LV}
+
+fsarchiver savefs -j${CPU_COUNT} -z${COMPRESSION_LEVEL} -o ${BACKUP_DIR}/${BACKUP_NAME} /dev/${LVM_ROOT_FS_VG}/${SNAP_NAME}
+RC=$?;
+if [[ ${RC} != 0 ]]; then
+  echo "Backup failed, removing incomplete file"
+  rm -f ${BACKUP_DIR}/${BACKUP_NAME}
+fi
+
+# Drop snapshot and remove physical volume
+lvremove --force /dev/${LVM_ROOT_FS_VG}/${SNAP_NAME}
+vgreduce ${LVM_ROOT_FS_VG} ${LOOPBACK_DEV}
+pvremove ${LOOPBACK_DEV}
+losetup -d ${LOOPBACK_DEV}
+rm ${TMP_LVM_SNAPSHOT_FILE}
+
+# Cleanup old backups (if current backup has been successfull)
+if [[ ${RC} == 0 ]]; then
+  # clean up daily archives
+  DAILY_COUNT=`find ${BACKUP_DIR} -name "${DAILY_PREFIX}*" | sort | wc -l`
+  let remove=$DAILY_COUNT-$DAILY_BACKUP_MAX_COUNT
+  if [ ${remove} -gt 0 ]; then
+    find ${BACKUP_DIR} -name "${DAILY_PREFIX}*" | sort | head -n ${remove} | xargs rm -f
+  fi
+  # clean up weekly archives
+  WEEKLY_COUNT=`find ${BACKUP_DIR} -name "${WEEKLY_PREFIX}*" | sort | wc -l`
+  let remove=$WEEKLY_COUNT-$WEEKLY_BACKUP_MAX_COUNT
+  if [ ${remove} -gt 0 ]; then
+    find ${BACKUP_DIR} -name "${WEEKLY_PREFIX}*" | sort | head -n ${remove} | xargs rm -f
+  fi
+fi
+
+# Return proper exit code
+exit ${RC}
