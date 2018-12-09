8bb579d50e462f8c5a35e50e49f2236addd4c61c
Dmitriusan <dmitriusan@gmail.com>
2018-12-09 14:40:12 +0200

    ^

--- tests/manage_backups/auto_clean/test_choose_valuable_backups.py
+++ tests/manage_backups/auto_clean/test_choose_valuable_backups.py
@@ -58,6 +58,32 @@ def test_should_return_limited_number_of_backups(mocker):
 
 
 # TODO: should work with small list
+def test_should_work_with_small_list(mocker):
+  """
+  Checks that method splits list of backups according to time periods
+  """
+  args = create_args(daily_backups_max_count=20, weekly_backups_max_count=10,
+                     monthly_backups_max_count=10, yearly_backups_max_count=10)
+  # Configuration
+  datetime_mock = mocker.patch('manage_backups.datetime')
+  datetime_mock.now = mocker.Mock(return_value=datetime(2018, 11, 14, 3, 14, 1))
+
+  list_of_backups = [
+    create_entry("20170312_041112"),
+    create_entry("20171114_031512"),
+    create_entry("20180407_031512"),
+    create_entry("20181014_031512"),
+    create_entry("20181023_022501"),
+    create_entry("20181106_011417"),
+    create_entry("20181110_031511"),
+    create_entry("20181113_031512")
+  ]
+  expected_number_of_chosen_backups = args.daily_backups_max_count + args.weekly_backups_max_count + \
+                                      args.monthly_backups_max_count + args.yearly_backups_max_count
+
+  # Run method under test
+  chosen_backups = manage_backups._choose_valuable_backups(list_of_backups, args)
+  assert chosen_backups == list_of_backups
 
 
 def create_entry(datetime_str):
